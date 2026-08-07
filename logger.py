685ac4aa7815f6414eba31a2b2388a3bbe53c020
Kaja Nowak <risn_erso@hotmail.com>
2026-08-07 11:10:16 +0100

    logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,16 +1,25 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='game.log', max_bytes=5 * 1024 * 1024, backup_count=3):
-    logger = logging.getLogger('game_logger')
-    logger.setLevel(logging.DEBUG)
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+
+def setup_logger(name='GameLogger', log_file='game.log', level=logging.INFO):
+    """Sets up a logger with rotation capability."""
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=2)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-    logger.addHandler(handler)
+
+    if not logger.hasHandlers():  
+        logger.addHandler(handler)
+
     return logger
 
-# Example usage
+
+# Example usage:
 if __name__ == '__main__':
-    logger = setup_logger()
-    logger.info('Logger is set up and running.')
+    log = setup_logger()
+    log.info('Logger is set up and ready!')
+    log.warning('This is a warning message.')
+    log.error('This is an error message.')
